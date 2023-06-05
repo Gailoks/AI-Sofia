d625266b4a8dbfe8ad7495326c8d3b8b9bb7b122
Gailoks <yra.cimonov@gmail.com>
2023-06-05 20:10:28 +0300

    module

--- RnnTextGen.py
+++ RnnTextGen.py
@@ -1,36 +1,29 @@
 import torch
 import torch.nn as nn
+import ServiceTokens as st
 
 
 class RnnTextGen(nn.Module):
 
-    def __init__(self, vocabulary_size, input_size, hid_size, n_layers, out_size, device, dropout=0.2) -> None:
-        super(RnnTextGen, self).__init__()
+    def __init__(self, vocabulary_size, input_size, hid_size, n_layers, dropout=0.2, device ='cpu') -> None:
+        super().__init__()
         self.n_layers = n_layers
         self.hidden_size = hid_size
-        self.out_size = out_size
-        self.Encoder = nn.Embedding(vocabulary_size, input_size)
-        self.l1 = nn.Linear(input_size, out_size)
-        self.Attention = nn.MultiheadAttention(out_size, 1)
-        self.lstm = nn.LSTM(input_size, hid_size, n_layers)
-        self.dropout = nn.Dropout(dropout)
-        self.l2 = nn.Linear(hid_size, out_size)
+        self.Encoder = nn.Embedding(vocabulary_size+st.SERVICE_INPUT_SIZE, input_size)
+        self.lstm = nn.LSTM(input_size, hid_size, n_layers, dropout = dropout)
+        self.l1 = nn.Linear(hid_size,vocabulary_size+st.SERVICE_OUTPUT_SIZE)
         self.device = device
 
     def forward(self, x, hidden=None):
-        x = self.Encoder(x)
-        p = self.l1(x)
-        aw, _ = self.Attention(p.view(-1, self.out_size), p.view(-1, self.out_size),
-                               p.view(-1, self.out_size))  # a - attn output b - attn_wheights)
-        x, hidden = self.lstm(x)
-        x = self.dropout(x)
-        x = self.l2(x)
-        x = aw*x
+        x = self.Encoder(x).detach()
+        x, hidden = self.lstm(x, hidden)
+        x = x[-1]
+        x = self.l1(x)
         return x, hidden
-
+    
     def init_hidden(self, batch_size=1):
         return (torch.zeros(self.n_layers, batch_size, self.hidden_size, requires_grad=True).to(self.device),
-                torch.zeros(self.n_layers, batch_size, self.hidden_size, requires_grad=True).to(self.device))
+               torch.zeros(self.n_layers, batch_size, self.hidden_size, requires_grad=True).to(self.device))
 
     def save(self, path):
         torch.save(self, path)
--- train.py
+++ train.py
@@ -2,67 +2,78 @@ import torch.nn as nn
 import torch
 import numpy as np
 import json
+import Dataset as ds
+import Tokens as tk
+import ServiceTokens as st
 
 
-def train(epoches: int, model: nn.Module, device: str, tokenizer, optimizer, scheduler, loss_func, dataset) -> None:
+def train(epoches: int, model: nn.Module, device: str, tokenizer, dataset) -> None:
     """epoches - number of epoches through all dataset
     model - model required to teach
     batch_size - n/a"""
-    def get_batch(dataset: list):
-        for qa in dataset:
-            question_idx = list(tokenizer.tokenize(qa.question))
-            target = list(tokenizer.tokenize(qa.answer)) + \
-                [tokenizer.count_tokens()]
-            test = question_idx+target[:-1]
-
-            target = torch.LongTensor(target).to(device)
-            test = torch.LongTensor(test).to(device)
-            yield target, test
-
-    loss_avg = []
-    model.train()
-    for epoch in range(epoches):
-        for target, train in get_batch(dataset):
-
-            hidden = model.init_hidden()
-
-            output, hidden = model(train, hidden)
-            target_len = len(target)
-            loss = loss_func(output[-target_len:], target)
-
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-
-            loss_avg.append(loss.item())
-            if len(loss_avg) >= 50:
-                mean_loss = np.mean(loss_avg)
-                print(f'Loss: {mean_loss}')
-                scheduler.step(mean_loss)
-                loss_avg = []
-
-
-if __name__ == "__main__":
-    import RnnTextGen
-    import Tokens as tk
-    import Dataset
-    with open("ai.json") as config:
-        parametrs = json.load(config)
-
-    tokens = tk.TokenDictionary.load("tokens.json")
-    tokenizer = tk.Tokenizer(tokens)
-    tokenizer.count_tokens
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    model = RnnTextGen.RnnTextGen(**parametrs, device=device).to(device)
-    optim = torch.optim.Adam(model.parameters(), lr=1e-2, amsgrad=True)
+    service_tokens = st.ServiceTokens(tokenizer.count_tokens())
+    loss_func = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, amsgrad=True)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optim,
+        optimizer,
         patience=5,
         verbose=True,
         factor=0.5
     )
-    loss_func = nn.CrossEntropyLoss()
-    train(40, model, device, tokenizer, optim,
-          scheduler, loss_func, Dataset.load())
+
+    def get_batch(dialog: ds.Dialog):
+        nninput = []
+        for qa in dialog.listPairs():
+            nninput += list(tokenizer.tokenize(qa.question)) + [service_tokens.get(st.STI_ROLE)]
+            nnexcept = list(tokenizer.tokenize(qa.answer)) + [service_tokens.get(st.STO_END)]
+            yield nninput.copy(), nnexcept.copy()
+            nninput += list(tokenizer.tokenize(qa.answer)) + [service_tokens.get(st.STI_ROLE)]
+
+    loss_avg = []
+    model.train()
+    for epoch in range(epoches):
+        for dialog in dataset.listDialogs():
+            for nninput, nnexcept in get_batch(dialog):
+                output, hidden = model(torch.LongTensor(nninput).to(device))
+                exceptTensor = torch.zeros(tokenizer.count_tokens() + st.SERVICE_OUTPUT_SIZE).to(device)
+                exceptTensor[nnexcept[0]] = 1.0
+                loss = loss_func(output, exceptTensor)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+                loss_avg.append(loss.item())
+
+                for target in nnexcept[1::]:
+                    output, hidden = model(output.argmax().view(-1), hidden)
+                    exceptTensor = torch.zeros(tokenizer.count_tokens() + st.SERVICE_OUTPUT_SIZE).to(device)
+                    exceptTensor[target] = 1.0
+
+                    loss = loss_func(output, exceptTensor)
+                    loss.backward()
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+                    loss_avg.append(loss.item())
+                if len(loss_avg) >= 50:
+                    mean_loss = np.mean(loss_avg)
+                    print(f'Loss: {mean_loss}')
+                    scheduler.step(mean_loss)
+                    loss_avg = []
+
+
+if __name__ == "__main__":
+    import RnnTextGen
+
+    with open("ai.json") as config:
+        parametrs = json.load(config)
+
+    tokens = tk.TokenDictionary.load(".aistate/tokens.json")
+    tokenizer = tk.Tokenizer(tokens)
+    tokenizer.count_tokens
+    device = 'cpu'#torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = RnnTextGen.RnnTextGen(**parametrs, device=device).to(device)
+    dataset = ds.load()
+
+    train(40, model, device, tokenizer, dataset)
     model.save('data.pkl')
