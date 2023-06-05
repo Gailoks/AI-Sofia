45a27c7b0ef7f33cd6a75091c3d05b475ba0546a
LloydLion <lloydlion2211@gmail.com>
2023-06-05 17:48:46 +0300

    Service tokens

---
+++ ServiceTokens.py
@@ -0,0 +1,9 @@
+STO_END = 0
+STI_ROLE = 0
+
+class ServiceTokens:
+    def __init__(self, vocabulary_size:int):
+        self.__vocabulary_size = vocabulary_size
+
+    def get(self, service_index:int):
+        return self.__vocabulary_size + service_index
