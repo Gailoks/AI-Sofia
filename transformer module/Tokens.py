590dd692339a2a528099b92e65982e77a5b0ead7
LloydLion <lloydlion2211@gmail.com>
2023-07-17 02:12:57 +0300

    Tokens rework

--- transformer module/Tokens.py
+++ transformer module/Tokens.py
@@ -1,23 +1,43 @@
 import json
 import Dataset as ds
+from collections import Counter
+from pymorphy3 import MorphAnalyzer
 # TokenDictionary, TokenDictionaryGenerator and Tokenizer
+
+
+VOCABULARY_TOKENS_COUNT = 1
+VT_UNKOWN = 0
+
+def preprocess_text(text: str):
+    text = text.lower()
+    text = "".join(filter(lambda x: x not in ",.-:\'\"@#$%^&*()_+=/*+?!", text))
+    return text
+
+def split_text(text: str, morph: MorphAnalyzer):
+    def normalize_word(x):
+        parse_result = morph.parse(x)
+        most_scored = max(parse_result, key=lambda x: x.score)
+        return most_scored.normalized.word
+
+    return list(map(normalize_word, preprocess_text(text).split()))
 
 
 class TokenDictionary():
     def __init__(self, tokens_decode_table: list):
-        self.tokens = sorted(tokens_decode_table, key = lambda x: len(x), reverse = True)
+        self.tokens = tokens_decode_table
         self.rw_tokens = {v: k for k, v in enumerate(self.tokens)}
 
     def code(self, text):
         for token, token_text in enumerate(self.tokens):
-            if text.startswith(token_text):
-                char_ate = len(token_text)
-                return token, char_ate
+            if text == token_text:
+                return token + VOCABULARY_TOKENS_COUNT
         else:
-            return -1, 0
+            return VT_UNKOWN
 
     def decode(self, token):
-        return self.tokens[token]
+        if token == VT_UNKOWN:
+            return None
+        return self.tokens[token - VOCABULARY_TOKENS_COUNT]
 
     def count(self):
         return len(self.tokens)
@@ -32,78 +52,58 @@ class TokenDictionary():
         with open(path, 'r') as file:
             tokens = json.load(file)
         return TokenDictionary(tokens)
+    
+    def __str__(self) -> str:
+        return "TokenDictionary" + str(self.tokens)
 
 
 class TokenDictionaryGenerator():
-    """
-    Accept options:\n
-    vocabulary_size (required) - target tokens dictionary size\n
-    token_depth (default: 5) - max lenght of token in statistic analize\n
-    use_words (default: True) - also analize full words
-    """
-
-    def __init__(self, vocabulary_size:int, token_depth:int = 5, use_words:bool = True):
-        self.__vocabulary_size = vocabulary_size
-        self.__token_depth = token_depth
-        self.__use_words = use_words
+    def __init__(self, vocabulary_size:int):
+        self.__vocabulary_size_fixed = vocabulary_size - VOCABULARY_TOKENS_COUNT
 
     def generate_tokens(self, samples: ds.Dataset) -> TokenDictionary:
-        tokens = []
 
-        # Get unique letter in samples and add they as tokens
-        alphabet = set()
+        tokens = [',', '.', '-', '?']
+
+        data = []
+
+        morph = MorphAnalyzer()
+
         for qa in samples.listPairs():
-            alphabet = alphabet.union(qa.question)
-            alphabet = alphabet.union(qa.answer)
+            data += split_text(qa.question, morph)
+            data += split_text(qa.answer, morph)
 
-        for char in alphabet:
-            tokens.append(char)
-
-        # Generate token using statistic based algoritm
-        def increase(s_dict, key):
-            s_dict[key] = s_dict.get(key, 0) + len(key)
-
-        def increase_limit(s_dict, key, limit):
-            if len(key) >= limit:
-                increase(s_dict, key)
-
-        token_depth = self.__token_depth
-        s_dict = {}
-
-        sample = " ".join(map(lambda qa: qa.question + " " + qa.answer, samples.listPairs()))
-        sample = "".join(filter(lambda x: x not in ",.?!", sample))
-
-        if self.__use_words:
-            [increase_limit(s_dict, word, token_depth)
-            for word in sample.split()]
-
-        for i in range(1, len(sample)):
-            for offset in range(1, token_depth):
-                increase(s_dict, sample[i - offset:i + 1])
-
-        length_of_tokens = len(tokens)
-        sds = sorted(s_dict.items(), key=lambda x: x[1], reverse=True)
-
-        # Copy first self.vocab_size - length_of_tokens from sds to token to fill token up to vocab_size
-        for i in range(self.__vocabulary_size - length_of_tokens):
-            tokens.append(sds[i][0])
+        counter = Counter(data)
+        tokens += list(map(lambda x: x[0], counter.most_common(self.__vocabulary_size_fixed - len(tokens))))
 
         return TokenDictionary(tokens)
 
 
 class Tokenizer():
-    def __init__(self, dictionary: TokenDictionary, **options):
+    def __init__(self, dictionary: TokenDictionary):
         self.dictionary = dictionary
-        self.options = options
+        self.morph = MorphAnalyzer()
 
     def tokenize(self, text:str) -> list():
-        while text:
-            token, char_ate = self.dictionary.code(text)
-            if token == -1:
-                text = text[1:]
-                continue
-            text = text[char_ate:]
-            yield token
+        result = []
+
+        for word in split_text(text, self.morph):
+            token = self.dictionary.code(word)
+
+            result.append(token)
+
+        return result
+    
+    def detokenize(self, tokens:list) -> str:
+        result = []
+
+        for token in tokens:
+            word = self.dictionary.decode(token)
+            if word == None:
+                word = "UNKNOWN"
+            result.append(word)
+
+        return " ".join(result)
 
     def decode_token(self, token:int):
         return self.dictionary.decode(token)
@@ -120,18 +120,17 @@ if __name__ == "__main__":
     import Dataset as ds
     from termcolor import colored
 
-
     dataset = ds.load()
 
-    generator = tk.TokenDictionaryGenerator(
-        vocabulary_size = 1200,
-        token_depth = 5,
-        use_words = True
-    )
+    generator = tk.TokenDictionaryGenerator(200)
 
     tokens = generator.generate_tokens(dataset)
 
     path = ".aistate/tokens.json"
     tokens.save(path)
 
+
     print(colored(f"Tokens were generated and saved into '{path}'", "green"))
+
+    print(colored(f"Tokens: {tokens}", "cyan"))
+
